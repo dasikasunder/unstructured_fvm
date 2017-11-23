08e4f8aecb9845ecccf0fc3b9d4c45b72e702df2
dasikasunder <dasikasunder@gmail.com>
2017-11-23 09:56:22 +0530

    GNU Octave plotting commands

--- surf_plot.m
+++ surf_plot.m
@@ -3,24 +3,27 @@
 ## Created: 2017-11-09
 
 function  surf_plot (plot)
- M = load("plot.txt");
- x = M(:,1);
- y = M(:,2);
- phi = M(:,3);
+    M = load("plot.txt");
+    x = M(:,1);
+    y = M(:,2);
+    phi = M(:,3);
 
  
-xlin=linspace(min(x),max(x),200);
-ylin=linspace(min(y),max(y),200);
-[X,Y]=meshgrid(xlin,ylin);
-Z=griddata(x,y,phi,X,Y,'linear');
-mesh(X,Y,Z);
-axis tight; 
-grid off
-hold on
+    xlin=linspace(min(x),max(x),100);
+    ylin=linspace(min(y),max(y),100);
+    [X,Y]=meshgrid(xlin,ylin);
+    Z=griddata(x,y,phi,X,Y,'linear');
+    mesh(X,Y,Z);
+    axis tight; 
+    grid off
+    hold on
 
-surf(X,Y,Z)
-colormap jet
-shading interp
-colorbar
+    surf(X,Y,Z)
+    grid on;
+    view(0,90)
+    colormap jet
+    shading interp
+    colorbar
+    print -djpg figure.jpg
 endfunction
  
