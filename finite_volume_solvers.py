292f431924df9e00388274a57785172782ed90d4
dasikasunder <dasikasunder@gmail.com>
2017-11-23 19:01:20 +0530

    added rk3, rk4 time stepping methods

--- finite_volume_solvers.py
+++ finite_volume_solvers.py
@@ -14,30 +14,52 @@ class advection_diffusion_equation_solver:
         self.u_0 = u_0
         self.dt = dt
         self.phi = phi
+        self.volume = np.zeros(self.phi.n_c)
+        
+        mesh = self.phi._ScalarField__mesh
+        
+        Cell_list = mesh.get_cell_list()
+        for i in range(0, self.phi.n_c):
+            self.volume[i] = Cell_list[i].area()
         
         self.A = np.matlib.zeros((self.phi.n_c, self.phi.n_c)) # Assembly matrix
         self.b = np.matlib.zeros((self.phi.n_c, 1)) # RHS vector
 
-
-            
+    
     def solve_using_explicit_euler_method(self):
         
-        mesh = self.phi._ScalarField__mesh 
-        Cell_list = mesh.get_cell_list()
-        laplace = fvc.laplacian(self.gamma, self.phi)
-        divergence = fvc.div(self.u_0, self.phi)
-        extra_term = np.zeros(self.phi.n_c)
-        
-        for i in range(0, self.phi.n_c):
-            Cell = Cell_list[i]
-            volume = Cell.area()
-            extra_term[i] = self.phi.cell_centroid_value[i]*volume - self.dt*divergence[i] + self.dt*laplace[i]
-            self.phi.cell_centroid_value[i] = extra_term[i]/volume
-        
+        L = self.dt*(-fvc.div(self.u_0, self.phi) + fvc.laplacian(self.gamma, self.phi))/self.volume
+        self.phi.cell_centroid_value += L
+          
+    def solve_using_rk3_tvd_method(self):
+        phi_1 = self.phi
+        phi_2 = self.phi
+        L1 = self.dt*(-fvc.div(self.u_0, self.phi) + fvc.laplacian(self.gamma, self.phi))/self.volume
+        phi_1.cell_centroid_value = self.phi.cell_centroid_value + L1
+        L2 = self.dt*(-fvc.div(self.u_0, phi_1) + fvc.laplacian(self.gamma, phi_1))/self.volume
+        phi_2.cell_centroid_value = (3./4.)*(self.phi.cell_centroid_value) + (1./4.)*(phi_1.cell_centroid_value) + (1./4.)*L2
+        L3 = self.dt*(-fvc.div(self.u_0, phi_2) + fvc.laplacian(self.gamma, phi_2))/self.volume    
+        self.phi.cell_centroid_value = (1./3.)*self.phi.cell_centroid_value + (2./3.)*(phi_2.cell_centroid_value) + (2./3.)*L3    
             
-                     
-                     
-                
-            
-                
+    def solve_using_rk4_method(self):
+        phi_1 = self.phi
+        phi_2 = self.phi
+        phi_3 = self.phi
+
+        L1 = self.dt*(-fvc.div(self.u_0, self.phi) + fvc.laplacian(self.gamma, self.phi))/self.volume
+
+        phi_1.cell_centroid_value  = self.phi.cell_centroid_value + (1./4.)*L1
+
+        L2 = self.dt*(-fvc.div(self.u_0, phi_1) + fvc.laplacian(self.gamma, phi_1))/self.volume
+
+        phi_2.cell_centroid_value  = self.phi.cell_centroid_value + (1./3.)*L2
+        
+        L3 = self.dt*(-fvc.div(self.u_0, phi_2) + fvc.laplacian(self.gamma, phi_2))/self.volume
+        
+        phi_3.cell_centroid_value  = self.phi.cell_centroid_value + (1./2.)*L3
+        
+        L4 = self.dt*(-fvc.div(self.u_0, phi_3) + fvc.laplacian(self.gamma, phi_3))/self.volume
+        
+        self.phi.cell_centroid_value = self.phi.cell_centroid_value + L4
+        
         
